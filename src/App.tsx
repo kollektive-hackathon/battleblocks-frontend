17a6f69cd2b25e62c8bff1b0e990cf9e8575ab2f
Filip Štoković <filip.stokovic@gmail.com>
2023-02-22 21:04:27 +0100

    fix: fix flicker while checking local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import { useEffect, useState } from 'react'
 import { Outlet } from 'react-router-dom'
 
+import Loader from '@/components/Loader.comp'
 import Login from '@/pages/Login.page'
 
 export default function App() {
-    const [isLoggedIn, setIsLoggedIn] = useState(false)
+    const [isLoggedIn, setIsLoggedIn] = useState<boolean | null>(null)
 
     useEffect(() => {
         const storedToken = localStorage.getItem('token') ? JSON.parse(localStorage.getItem('token')!) : null
 
         // TODO: check token and log user in if it's valid
+        // also move state to context
         setIsLoggedIn(!!storedToken)
     }, [])
+
+    if (isLoggedIn === null) {
+        return <Loader />
+    }
 
     if (!isLoggedIn) {
         return <Login />
