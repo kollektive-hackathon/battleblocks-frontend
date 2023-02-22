01835a671a1686650562e4a2070194ae10f95e0e
Filip Štoković <filip.stokovic@gmail.com>
2023-02-22 14:40:00 +0100

    feat: add login page and style reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
-const storedUser = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')!) : null
+import { useEffect, useState } from 'react'
+
+import Login from './pages/Login.page'
 
 export default function App() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+    useEffect(() => {
+        const storedToken = localStorage.getItem('token') ? JSON.parse(localStorage.getItem('token')!) : null
+
+        // TODO: check token and log user in if it's valid
+    }, [])
+
+    if (!isLoggedIn) {
+        return <Login />
+    }
+
     return <div className="app">battleblocks</div>
 }
--- src/pages/Login.page.tsx
+++ src/pages/Login.page.tsx
@@ -1,3 +1,28 @@
 export default function Login() {
-    return <>nispe</>
+    return (
+        <div className="login">
+            <div className="login__content">
+                <div className="login__title">
+                    battlebl
+                    <div className="white-square" />
+                    cks
+                </div>
+                <div className="login__message">continue?with&gt;</div>
+                <div className="login__container">
+                    <div className="login__container__app login__container__app--google">
+                        <img src="/icons/google.svg" alt="Google login" />
+                    </div>
+                    <div className="login__container__app login__container__app--apple">
+                        <img src="/icons/apple.svg" alt="Apple login" />
+                    </div>
+                </div>
+            </div>
+            <div className="login__footer">
+                BATTLEBLOCKS is an open-source decentralized application developed by the team of the same name as part
+                of the FLOW Hackathon 2023, held from February 21st to 26th. The dApp is not affiliated with any other
+                organizations or entities and is provided &quot;as is&quot; without any warranties of any kind, express
+                or implied. Your use of the dApp is entirely at your own risk and under your own discretion.
+            </div>
+        </div>
+    )
 }
