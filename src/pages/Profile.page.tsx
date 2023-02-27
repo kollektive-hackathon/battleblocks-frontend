dc84d5ef2ac2beb58c030cf4b19661cd59611e2a
Filip Štoković <filip.stokovic@gmail.com>
2023-02-27 16:12:17 +0100

    fix(profile): loader

--- src/pages/Profile.page.tsx
+++ src/pages/Profile.page.tsx
@@ -14,66 +14,62 @@ export default function Profile() {
         removeTokenAndUser()
     }, [])
 
-    return (
+    return user ? (
         <div className="profile page-container">
-            {user ? (
-                <>
-                    <div className="page-container__title">
-                        <div className="page-container__title__value">profile // {user.username}</div>
-                        <div className="logout" onClick={() => logout()}>
-                            logout //
-                        </div>
+            <div className="page-container__title">
+                <div className="page-container__title__value">profile // {user.username}</div>
+                <div className="logout" onClick={() => logout()}>
+                    logout //
+                </div>
+            </div>
+            <div className="page-container__content">
+                <div className="profile__wallet-container">
+                    <div className="profile__wallet-container__battleblocks">
+                        battleblocks-wallet:{' '}
+                        <span className="wallet-container__address">{user.custodialWalletAddress}</span>
                     </div>
-                    <div className="page-container__content">
-                        <div className="profile__wallet-container">
-                            <div className="profile__wallet-container__battleblocks">
-                                battleblocks-wallet:{' '}
-                                <span className="wallet-container__address">{user.custodialWalletAddress}</span>
-                            </div>
-                            {user.selfCustodyWalletAddress || bloctoUser?.addr ? (
-                                <div className="profile__wallet-container__connected">
-                                    personal-wallet:{' '}
-                                    <span className="wallet-container__address">
-                                        {user.selfCustodyWalletAddress ?? bloctoUser?.addr}
-                                    </span>
-                                </div>
-                            ) : (
-                                <div className="profile__wallet-container__connect" onClick={() => cosign()}>
-                                    personal-wallet?connect &gt;&gt;
-                                </div>
-                            )}
+                    {user.selfCustodyWalletAddress || bloctoUser?.addr ? (
+                        <div className="profile__wallet-container__connected">
+                            personal-wallet:{' '}
+                            <span className="wallet-container__address">
+                                {user.selfCustodyWalletAddress ?? bloctoUser?.addr}
+                            </span>
                         </div>
-                        <table className="page-container__content__table">
-                            <thead>
-                                <tr>
-                                    <th>my inventory</th>
-                                    <th>block type</th>
-                                    <th>rarity</th>
-                                    <th>active</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {user.inventoryBlocks.map((block) => (
-                                    <tr
-                                        key={block.id}
-                                        className={`table-item${!block.active ? ' table-item--deactivated' : ''}`}
-                                        onClick={() => {
-                                            // TODO: toggle active
-                                        }}
-                                    >
-                                        <td className="table-item__property">{block.name}</td>
-                                        <td className="table-item__property">{block.type}</td>
-                                        <td className="table-item__property">{block.rarity}</td>
-                                        <td className="table-item__property">{block.active ? '+' : '-'}</td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </div>
-                </>
-            ) : (
-                <Loader dark />
-            )}
+                    ) : (
+                        <div className="profile__wallet-container__connect" onClick={() => cosign()}>
+                            personal-wallet?connect &gt;&gt;
+                        </div>
+                    )}
+                </div>
+                <table className="page-container__content__table">
+                    <thead>
+                        <tr>
+                            <th>my inventory</th>
+                            <th>block type</th>
+                            <th>rarity</th>
+                            <th>active</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {user.inventoryBlocks.map((block) => (
+                            <tr
+                                key={block.id}
+                                className={`table-item${!block.active ? ' table-item--deactivated' : ''}`}
+                                onClick={() => {
+                                    // TODO: toggle active
+                                }}
+                            >
+                                <td className="table-item__property">{block.name}</td>
+                                <td className="table-item__property">{block.type}</td>
+                                <td className="table-item__property">{block.rarity}</td>
+                                <td className="table-item__property">{block.active ? '+' : '-'}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
+    ) : (
+        <Loader />
     )
 }
