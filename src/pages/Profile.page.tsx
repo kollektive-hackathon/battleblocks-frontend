710cf5c2b1b9ecded63f3e56bc2a8971444a8da0
Luka Racki <luka@racki.dev>
2023-02-28 01:03:11 +0100

    feat: add loading for cosigning

--- src/pages/Profile.page.tsx
+++ src/pages/Profile.page.tsx
@@ -1,121 +1,128 @@
-import { useCallback, useEffect } from 'react'
-import axios from 'axios'
+import { useCallback, useEffect, useState } from 'react';
+import axios from 'axios';
 
-import Loader from '@/components/Loader.comp'
-import { useNotificationContext } from '@/context/NotificationContext'
-import { useUserContext } from '@/context/UserContext'
-import { cosign } from '@/flow/cosign.tx'
-import { removeTokenAndUser } from '@/utils/token'
+import Loader from '@/components/Loader.comp';
+import { useNotificationContext } from '@/context/NotificationContext';
+import { useUserContext } from '@/context/UserContext';
+import { cosign } from '@/flow/cosign.tx';
+import { removeTokenAndUser } from '@/utils/token';
 
 export default function Profile() {
-    const { bloctoUser, setUser, user } = useUserContext()
-    const { setNotification } = useNotificationContext()
+  const { bloctoUser, setUser, user } = useUserContext();
+  const { setNotification } = useNotificationContext();
+  const [custodialWallet, setCustodialWallet] = useState<string>('');
 
-    const logout = useCallback(() => {
-        setUser(null)
+  const [connnectWalletMessage, setConnnectWalletMessage] = useState('personal-wallet?connect >>');
+  const logout = useCallback(() => {
+    setUser(null);
 
-        removeTokenAndUser()
-    }, [])
+    removeTokenAndUser();
+  }, []);
 
-    const copyToClipboard = useCallback(
-        (address: string) => {
-            navigator.clipboard.writeText(address).then(() => {
-                setNotification({
-                    title: 'wallet-address',
-                    description: 'successfully copied to clipboard'
-                })
-            })
-        },
-        [setNotification]
-    )
+  const copyToClipboard = useCallback(
+    (address: string) => {
+      navigator.clipboard.writeText(address).then(() => {
+        setNotification({
+          title: 'wallet-address',
+          description: 'successfully copied to clipboard',
+        });
+      });
+    },
+    [setNotification]
+  );
 
-    useEffect(() => {
-        axios
-            .get('/profile')
-            .then((result) => setUser(result.data))
-            .catch(() =>
-                setNotification({
-                    title: 'profile-error',
-                    description: 'error fetching profile'
-                })
-            )
-    }, [])
+  const sendCosignTx = async () => {
+    let result;
 
-    return user ? (
-        <div className="profile page-container">
-            <div className="page-container__title">
-                <div className="page-container__title__value">profile // {user.username}</div>
-                <div className="logout" onClick={() => logout()}>
-                    logout //
-                </div>
-            </div>
-            <div className="page-container__content">
-                <div className="profile__wallet-container">
-                    <div className="profile__wallet-container__battleblocks">
-                        battleblocks-wallet:{' '}
-                        <span
-                            className="wallet-container__address"
-                            onClick={() => copyToClipboard(user.custodialWalletAddress)}
-                        >
-                            {user.custodialWalletAddress}
-                        </span>
-                    </div>
-                    {user.selfCustodyWalletAddress ? (
-                        <div className="profile__wallet-container__connected">
-                            personal-wallet:{' '}
-                            <span
-                                className="wallet-container__address"
-                                onClick={
-                                    () =>
-                                        user.selfCustodyWalletAddress ||
-                                        (bloctoUser?.addr &&
-                                            copyToClipboard(user.selfCustodyWalletAddress || bloctoUser?.addr))
-                                    // eslint-disable-next-line react/jsx-curly-newline
-                                }
-                            >
-                                {user.selfCustodyWalletAddress || bloctoUser?.addr}
-                            </span>
-                        </div>
-                    ) : (
-                        <div
-                            className="profile__wallet-container__connect"
-                            onClick={() => cosign(user.custodialWalletAddress)}
-                        >
-                            personal-wallet?connect &gt;&gt;
-                        </div>
-                    )}
-                </div>
-                {!!user.inventoryBlocks && (
-                    <table className="page-container__content__table">
-                        <thead>
-                            <tr>
-                                <th>my inventory</th>
-                                <th>block type</th>
-                                <th>rarity</th>
-                                <th>active</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {user.inventoryBlocks.map((block) => (
-                                <tr
-                                    key={block.id}
-                                    className={`table-item${!block.active ? ' table-item--deactivated' : ''}`}
-                                    onClick={() => {
-                                        // TODO: toggle active
-                                    }}
-                                >
-                                    <td className="table-item__property">{block.name}</td>
-                                    <td className="table-item__property">{block.type}</td>
-                                    <td className="table-item__property">{block.rarity}</td>
-                                    <td className="table-item__property">{block.active ? '+' : '-'}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                )}
-            </div>
+    if (connnectWalletMessage !== 'Connecting...') {
+      await setConnnectWalletMessage('Connecting...');
+
+      result = await cosign(user?.custodialWalletAddress!);
+    }
+
+    if (result) {
+      setCustodialWallet(bloctoUser?.addr!);
+    }
+  };
+
+  useEffect(() => {
+    axios
+      .get('/profile')
+      .then((result) => setUser(result.data))
+      .catch(() =>
+        setNotification({
+          title: 'profile-error',
+          description: 'error fetching profile',
+        })
+      );
+  }, []);
+
+  return user ? (
+    <div className="profile page-container">
+      <div className="page-container__title">
+        <div className="page-container__title__value">profile // {user.username}</div>
+        <div className="logout" onClick={() => logout()}>
+          logout //
         </div>
-    ) : (
-        <Loader />
-    )
+      </div>
+      <div className="page-container__content">
+        <div className="profile__wallet-container">
+          <div className="profile__wallet-container__battleblocks">
+            battleblocks-wallet:{' '}
+            <span className="wallet-container__address" onClick={() => copyToClipboard(user.custodialWalletAddress)}>
+              {user.custodialWalletAddress}
+            </span>
+          </div>
+          {user.selfCustodyWalletAddress || custodialWallet ? (
+            <div className="profile__wallet-container__connected">
+              personal-wallet:{' '}
+              <span
+                className="wallet-container__address"
+                onClick={() =>
+                  (user.selfCustodyWalletAddress || custodialWallet) &&
+                  copyToClipboard(user.selfCustodyWalletAddress || custodialWallet)
+                }
+              >
+                {user.selfCustodyWalletAddress || custodialWallet}
+              </span>
+            </div>
+          ) : (
+            <div className="profile__wallet-container__connect" onClick={() => sendCosignTx()}>
+              {connnectWalletMessage}
+            </div>
+          )}
+        </div>
+        {!!user.inventoryBlocks && (
+          <table className="page-container__content__table">
+            <thead>
+              <tr>
+                <th>my inventory</th>
+                <th>block type</th>
+                <th>rarity</th>
+                <th>active</th>
+              </tr>
+            </thead>
+            <tbody>
+              {user.inventoryBlocks.map((block) => (
+                <tr
+                  key={block.id}
+                  className={`table-item${!block.active ? ' table-item--deactivated' : ''}`}
+                  onClick={() => {
+                    // TODO: toggle active
+                  }}
+                >
+                  <td className="table-item__property">{block.name}</td>
+                  <td className="table-item__property">{block.type}</td>
+                  <td className="table-item__property">{block.rarity}</td>
+                  <td className="table-item__property">{block.active ? '+' : '-'}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
+    </div>
+  ) : (
+    <Loader />
+  );
 }
