4aac7cd25817a2ed7111c61ec46f3fcaf28bbdc8
Luka Racki <luka@racki.dev>
2023-03-01 01:46:09 +0100

    fix: add 1 to stake

--- src/components/CreateMatchModal.comp.tsx
+++ src/components/CreateMatchModal.comp.tsx
@@ -1,69 +1,69 @@
-import { useCallback, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useCallback, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import { useNotificationContext } from '@/context/NotificationContext'
-import { useUserContext } from '@/context/UserContext'
-import { fetchBalance } from '@/flow/fetchBalance.script'
+import { useNotificationContext } from '@/context/NotificationContext';
+import { useUserContext } from '@/context/UserContext';
+import { fetchBalance } from '@/flow/fetchBalance.script';
 
 type Props = {
-    closeModal: () => void
-}
+  closeModal: () => void;
+};
 
 export default function CreateMatchModal(props: Props) {
-    const { closeModal } = props
-    const [stake, setStake] = useState<number>()
-    const { setNotification } = useNotificationContext()
-    const { user } = useUserContext()
-    const navigate = useNavigate()
+  const { closeModal } = props;
+  const [stake, setStake] = useState<number>();
+  const { setNotification } = useNotificationContext();
+  const { user } = useUserContext();
+  const navigate = useNavigate();
 
-    const createMatch = useCallback(async () => {
-        const balance = await fetchBalance(user?.custodialWalletAddress ?? '')
-        if (balance < (stake ?? 0)) {
-            setNotification({
-                title: 'invalid-stake',
-                description: `you don't have enough FLOW`
-            })
+  const createMatch = useCallback(async () => {
+    const balance = await fetchBalance(user?.custodialWalletAddress ?? '');
+    if (balance < (stake ?? 0) + 1) {
+      setNotification({
+        title: 'invalid-stake',
+        description: `you don't have enough FLOW`,
+      });
 
-            return
-        }
+      return;
+    }
 
-        if (!stake || stake <= 0) {
-            setNotification({
-                title: 'invalid-stake',
-                description: 'stake must be greater than 0'
-            })
+    if (!stake || stake <= 0) {
+      setNotification({
+        title: 'invalid-stake',
+        description: 'stake must be greater than 0',
+      });
 
-            return
-        }
+      return;
+    }
 
-        navigate('/game/new', { state: { stake } })
+    navigate('/game/new', { state: { stake } });
 
-        closeModal()
-    }, [stake, closeModal])
+    closeModal();
+  }, [stake, closeModal]);
 
-    return (
-        <div className="modal-backdrop" onClick={() => closeModal()}>
-            <div className="modal" onClick={(e) => e.stopPropagation()}>
-                <div className="modal__title">create match</div>
-                <div className="modal__stake">
-                    <input
-                        type="number"
-                        className="modal__input"
-                        placeholder="stake?amount"
-                        onChange={(event) => setStake(+event.target.value)}
-                        onKeyUp={(e) => {
-                            if (e.key === 'Enter') {
-                                createMatch()
-                            }
-                        }}
-                        autoFocus
-                    />
-                    flow
-                </div>
-                <div className="modal__cta" onClick={() => createMatch()}>
-                    finish &gt;&gt;
-                </div>
-            </div>
+  return (
+    <div className="modal-backdrop" onClick={() => closeModal()}>
+      <div className="modal" onClick={(e) => e.stopPropagation()}>
+        <div className="modal__title">create match</div>
+        <div className="modal__stake">
+          <input
+            type="number"
+            className="modal__input"
+            placeholder="stake?amount"
+            onChange={(event) => setStake(+event.target.value)}
+            onKeyUp={(e) => {
+              if (e.key === 'Enter') {
+                createMatch();
+              }
+            }}
+            autoFocus
+          />
+          flow
         </div>
-    )
+        <div className="modal__cta" onClick={() => createMatch()}>
+          finish &gt;&gt;
+        </div>
+      </div>
+    </div>
+  );
 }
